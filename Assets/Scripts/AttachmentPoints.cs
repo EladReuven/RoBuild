49c03e1e0bf64a94c7ad4ca86171d1fb4455ed15
EladReuven <102731503+EladReuven@users.noreply.github.com>
2022-08-05 11:27:46 +0300

    added random spawn and some artless shapes

---
+++ Assets/Scripts/AttachmentPoints.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachmentPoints : MonoBehaviour
+{
+    [SerializeField] Transform topPoint;
+    [SerializeField] Transform bottomPoint;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer m_MeshRenderer;
+    [SerializeField] List<GameObject> shapesPrefabs;
+    [SerializeField] List<GameObject> possibleShapes;
+    [SerializeField] List<GameObject> onScreen = new List<GameObject>();
+
+    public int shapesPerScreen;
+
+    private void Awake()
+    {
+        InitShapes();
+    }
+
+    private void Start()
+    {
+        Debug.Log(possibleShapes.Count);
+        SpawnShapes();
+    }
+
+
+
+    private void SpawnShapes()
+    {
+        int rand;
+        for(int i = 0; i < shapesPerScreen; i++)
+        {
+            rand = Random.Range(0, possibleShapes.Count);
+            AddToScreen(possibleShapes[rand]);
+        }
+    }
+
+    private void AddToScreen(GameObject shape)
+    {
+        onScreen.Add(shape);
+        possibleShapes.Remove(shape);
+        shape.transform.position = MeshRendererBounds();
+        shape.SetActive(true);
+    }
+
+    private void RemoveFromScreen(GameObject shape)
+    {
+        possibleShapes.Add(shape);
+        onScreen.Remove(shape);
+        shape.SetActive(false);
+
+    }
+
+    private Vector3 MeshRendererBounds()
+    {
+        Vector3 spawnLocation = new Vector3(Random.Range(m_MeshRenderer.bounds.min.x, m_MeshRenderer.bounds.max.x), Random.Range(m_MeshRenderer.bounds.min.y, m_MeshRenderer.bounds.max.y),0);
+        return spawnLocation;
+    }
+
+    private void InitShapes()
+    {
+        possibleShapes = new List<GameObject>();
+        for(int i = 0; i < shapesPrefabs.Count; i++)
+        {
+            GameObject newShape = Instantiate(shapesPrefabs[i]);
+            newShape.SetActive(false);
+            possibleShapes.Add(newShape);
+        }
+    }
+}
